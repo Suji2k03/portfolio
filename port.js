b846eee48ffbf0c7dfd4231e4c5d675a6f234ca7
Sujitha <sujitha2k03@gmail.com>
2024-11-29 11:47:31 +0530

    My portfolio

---
+++ port.js
@@ -0,0 +1,9 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener("click", function(e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth"
+      });
+    });
+  });
